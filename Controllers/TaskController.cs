b533f01415c7e601443bdfa011f4f8d4e4638cab
John Z <johnziss9@gmail.com>
2023-04-06 15:57:16 +0100

    Added methods for getting a single task and updating a task. Also made all the methods async.

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -20,10 +20,27 @@ namespace Task_Management.Controllers
             return Ok(await _taskService.GetAll());
         }
 
+        [Route("{id}")]
+        public async Task<IActionResult> GetSingle(string id)
+        {
+            return Ok(await _taskService.GetSingle(id));
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddTask(Models.Task task)
         {
             return Ok(await _taskService.AddTask(task));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(string id, Models.Task updatedTask)
+        {
+            var taskResponse = await _taskService.GetSingle(id);
+
+            if (taskResponse.Data == null)
+                return NotFound(taskResponse);
+
+            return Ok(await _taskService.UpdateTask(id, updatedTask));
         }
     }
 }
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -11,7 +11,7 @@ namespace Task_Management.Services
         public TaskService(IConfiguration config, IDatabaseSettings databaseSettings)
         {
             _config = config;
-            
+
             var client = new MongoClient(_config["AppSettings:DatabaseSettings:ConnectionString"]);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
             _tasks = database.GetCollection<Models.Task>(databaseSettings.TasksCollectionName);
@@ -20,8 +20,17 @@ namespace Task_Management.Services
         public async Task<ServiceResponse<List<Models.Task>>> GetAll()
         {
             ServiceResponse<List<Models.Task>> serviceResponse = new ServiceResponse<List<Models.Task>>();
-            List<Models.Task> tasks = _tasks.Find(tasks => true).ToList(); 
+            List<Models.Task> tasks = await _tasks.Find(tasks => true).ToListAsync();
             serviceResponse.Data = tasks;
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<Models.Task>> GetSingle(string id)
+        {
+            ServiceResponse<Models.Task> serviceResponse = new ServiceResponse<Models.Task>();
+            Models.Task task = await _tasks.Find<Models.Task>(t => t.Id == id).FirstOrDefaultAsync();
+            serviceResponse.Data = task;
 
             return serviceResponse;
         }
@@ -29,9 +38,28 @@ namespace Task_Management.Services
         public async Task<ServiceResponse<List<Models.Task>>> AddTask(Models.Task task)
         {
             ServiceResponse<List<Models.Task>> serviceResponse = new ServiceResponse<List<Models.Task>>();
-            _tasks.InsertOne(task);
-            List<Models.Task> tasks = _tasks.Find(task => true).ToList();
+            await _tasks.InsertOneAsync(task);
+            List<Models.Task> tasks = await _tasks.Find(task => true).ToListAsync();
             serviceResponse.Data = tasks;
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<Models.Task>> UpdateTask(string id, Models.Task updatedTask)
+        {
+            ServiceResponse<Models.Task> serviceResponse = new ServiceResponse<Models.Task>();
+
+            try
+            {
+                await _tasks.ReplaceOneAsync(t => t.Id == id, updatedTask);
+                Models.Task task = await _tasks.Find<Models.Task>(t => t.Id == id).FirstOrDefaultAsync();
+                serviceResponse.Data = task;
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
 
             return serviceResponse;
         }

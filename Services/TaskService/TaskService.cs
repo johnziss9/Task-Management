67b84d02c1b9b80dc6709a1f925c7dd6788d322e
John Z <johnziss9@gmail.com>
2023-05-23 12:18:22 +0100

    When adding or updating a task, the code now searches for all TashHistory items and adds them to the History field. Also tidied up the Update method.

--- Services/TaskService/TaskService.cs
+++ Services/TaskService/TaskService.cs
@@ -46,6 +46,14 @@ namespace Task_Management.Services.TaskService
             ServiceResponse<List<Models.Task>> serviceResponse = new ServiceResponse<List<Models.Task>>();
             await _tasks.InsertOneAsync(task);
             await AddTaskToHistory(new TaskHistory { TaskId = task.Id, Description = "Task Created", DateModified = DateTime.Now });
+
+            // Retrieve all TaskHistory items and add them to the History field
+            List<TaskHistory> taskHistory = await _taskHistory.Find(item => item.TaskId == task.Id).ToListAsync();
+            task.History = taskHistory;
+
+            // Save the modified task back to the _tasks collection
+            await _tasks.ReplaceOneAsync(t => t.Id == task.Id, task);
+
             List<Models.Task> tasks = await _tasks.Find(task => true).ToListAsync();
             serviceResponse.Data = tasks;
 
@@ -58,7 +66,6 @@ namespace Task_Management.Services.TaskService
 
             try
             {
-                // Check if update includes a value for assignedTo property
                 Models.Task existingTask = await _tasks.Find<Models.Task>(t => t.Id == id).FirstOrDefaultAsync();
 
                 if (existingTask == null)
@@ -69,31 +76,37 @@ namespace Task_Management.Services.TaskService
                     return serviceResponse;
                 }
 
-                if (existingTask.AssignedTo?.Id != updatedTask.AssignedTo?.Id && updatedTask.AssignedTo != null)
+                bool assignedToChanged = existingTask.AssignedTo?.Id != updatedTask.AssignedTo?.Id;
+                bool statusChanged = existingTask.Status != updatedTask.Status;
+
+                if (assignedToChanged && updatedTask.AssignedTo != null)
                     _producer.SendMessage<Models.Task>(updatedTask);
-                
-                if (existingTask.AssignedTo?.Id != updatedTask.AssignedTo?.Id)
+
+                if (assignedToChanged || statusChanged)
                 {
                     var currentUser = existingTask.AssignedTo == null ? "Unassigned" : existingTask.AssignedTo.Username;
                     var updatedUser = updatedTask.AssignedTo == null ? "Unassigned" : updatedTask.AssignedTo.Username;
 
+                    string description = string.Empty;
+
+                    if (assignedToChanged && statusChanged)
+                        description = $"Task has been assigned from {currentUser} to {updatedUser} and status has changed from {existingTask.Status} to {updatedTask.Status}";
+                    else if (assignedToChanged)
+                        description = $"Task has been assigned from {currentUser} to {updatedUser}";
+                    else if (statusChanged)
+                        description = $"Task status has changed from {existingTask.Status} to {updatedTask.Status}";
+
                     await AddTaskToHistory(new TaskHistory
                     {
                         TaskId = existingTask.Id,
                         DateModified = DateTime.Now,
-                        Description = $"Task has been assigned from {currentUser} to {updatedUser}"
-                    });
+                        Description = description
+                    });                    
                 }
 
-                if (existingTask.Status != updatedTask.Status)
-                {
-                    await AddTaskToHistory(new TaskHistory
-                    {
-                        TaskId = existingTask.Id,
-                        DateModified = DateTime.Now,
-                        Description = $"Task status has changed from {existingTask.Status} to {updatedTask.Status}"
-                    });
-                }
+                // Retrieve all TaskHistory items and add them to the History field
+                List<TaskHistory> taskHistory = await _taskHistory.Find(item => item.TaskId == updatedTask.Id).ToListAsync();
+                updatedTask.History = taskHistory;
 
                 await _tasks.ReplaceOneAsync(t => t.Id == id, updatedTask);
 
